3a5c27e196160191ccf4fca4b5b7809ec71d8ff1
Tom Hodgkinson <hdgknsn@gmail.com>
2020-06-03 12:26:41 -0400

    fixed situation where header links were not updating on login/out

--- frontend/src/components/todo/Header.jsx
+++ frontend/src/components/todo/Header.jsx
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import AuthenticationService from "./AuthenticationService.js";
+// import AuthenticationService from "./AuthenticationService";
 
 class Header extends Component {
   render() {
-    const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
     return (
       <header>
         <nav className=" navbar navbar-expand-md navbar-dark bg-dark">
           <h2 className="navbar-brand">
-            <a href="welcome">LibLogic</a>
+            <Link to="/welcome">LibLogic</Link>
           </h2>
           <ul className="navbar-nav">
-            {isUserLoggedIn && (
+            {this.props.loggedInStatus && (
               <li>
                 <Link className="nav-link" to="/welcome">
                   Home
                 </Link>
               </li>
             )}
-            {isUserLoggedIn && (
+
+            {this.props.loggedInStatus && (
               <li>
                 <Link className="nav-link" to="/todos">
                   Todos
@@ -28,16 +28,19 @@ class Header extends Component {
             )}
           </ul>
           <ul className="navbar-nav navbar-collapse justify-content-end">
-            <li>
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-            {isUserLoggedIn && (
+            {!this.props.loggedInStatus && (
+              <li>
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+            )}
+
+            {this.props.loggedInStatus && (
               <li>
                 <Link
                   className="nav-link"
-                  onClick={AuthenticationService.logout}
+                  //        onClick={AuthenticationService.logout}
                   to="/logout"
                 >
                   Logout
--- frontend/src/components/todo/Login.jsx
+++ frontend/src/components/todo/Login.jsx
@@ -7,30 +7,27 @@ class Login extends Component {
     this.state = {
       username: "",
       password: "",
-      isLoggedIn: -1,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.loginClicked = this.loginClicked.bind(this);
   }
 
-  render() {
+  render(props) {
     return (
       <div className="container">
         <h1>Login</h1>
-        {!this.state.isLoggedIn && (
-          <div className="alert alert-warning">Invalid Credentials</div>
-        )}
         <form className="form-inline" style={{ display: "block" }}>
-          User Name:
+          {" "}
+          User Name:{" "}
           <input
             className="form-control"
             type="text"
             name="username"
             value={this.state.username}
             onChange={this.handleChange}
-          />
-          Password:
+          />{" "}
+          Password:{" "}
           <input
             className="form-control"
             type="password"
@@ -38,7 +35,11 @@ class Login extends Component {
             value={this.state.password}
             onChange={this.handleChange}
           />
-          <button className="btn btn-success" onClick={this.loginClicked}>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={this.loginClicked}
+          >
             Login
           </button>
         </form>
@@ -47,20 +48,19 @@ class Login extends Component {
   }
 
   loginClicked() {
-    if (this.state.username === "Tom" && this.state.password === "dummy") {
+    if (this.state.username === "Tom" && this.state.password === "pass") {
       AuthenticationService.registerUser(
         this.state.username,
         this.state.password
       );
-      this.props.history.push(`/welcome/${this.state.username}`);
-      this.setState({ isLoggedIn: true });
+      this.props.setLoggedInStatus(true);
+      this.props.history.push("/welcome");
     } else {
-      this.setState({ isLoggedIn: false });
+      this.props.setLoggedInStatus(false);
     }
   }
 
   handleChange(e) {
-    //register user
     this.setState({ [e.target.name]: e.target.value });
   }
 }
